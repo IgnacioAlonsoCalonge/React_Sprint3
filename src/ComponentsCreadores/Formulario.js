d556c5941d1f65e27eee79003a2dd19cd349f7fc
nj4funn@gmail.com <nj4funn@gmail.com>
2022-05-10 13:57:58 +0200

    Premium-gratuito

--- src/ComponentsCreadores/Formulario.js
+++ src/ComponentsCreadores/Formulario.js
@@ -29,7 +29,7 @@ export default function Formulario () {
         imagen: null,
         documento: null,
         publicado: 'false',
-        acceso: 'Demo',
+        acceso: 'gratuito',
         descripcion:''
     })
     
